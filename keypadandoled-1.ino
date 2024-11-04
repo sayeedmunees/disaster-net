b282b0d7ebabe14a1e85274734ea4dba09ac2bb4
Sayeed Munees <92251933+sayeedmunees@users.noreply.github.com>
2024-11-04 12:56:54 +0530

    Update keypadandoled-1.ino
    Removed servo names and tags

--- keypadandoled-1.ino
+++ keypadandoled-1.ino
@@ -1,11 +1,9 @@
 #include <Adafruit_GFX.h>
 #include <Adafruit_SSD1306.h>
 #include <Keypad.h>
-#include <ESP32Servo.h>
 const int rows=4;
 const int cols=4;
 Adafruit_SSD1306 display = Adafruit_SSD1306(128, 64, &Wire);
-Servo myservo;
 char keys[rows][cols] = {
   {'1', '2', '3', 'A'},
   {'4', '5', '6', 'B'},
@@ -18,9 +16,7 @@ uint8_t rowpins[cols] = {12, 14, 27, 26};
 Keypad keypad = Keypad(makeKeymap(keys), rowpins, colpins, rows, cols);
 const String password = "0000"; 
 String input_password;
-Servo servo_bin;
 void setup(){
-  servo_bin.attach(19);
   display.begin(SSD1306_SWITCHCAPVCC, 0x3C);
   display.setTextColor(WHITE,BLACK);
   display.setCursor(0,2);
@@ -36,7 +32,6 @@ void loop(){
   Serial.println(key);
   display.print(key);
   display.display();
-  
  if (key == '*') {
       input_password = ""; 
       display.clearDisplay();
@@ -45,7 +40,6 @@ void loop(){
   else if (key == '#') {
       if ("1" == input_password) {
         Serial.println("Help Me");
-        servo_bin.write(0);
         display.clearDisplay();
         display.setCursor(0,2);
         display.setTextSize(2);
@@ -55,7 +49,6 @@ void loop(){
         display.print("Help Me");
         display.display();
         delay(3000);
-        servo_bin.write(90);
         display.clearDisplay();
         display.display();
         display.setCursor(0,2);
